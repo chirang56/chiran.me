4dfda1a84d7ac673454a6afb7a060a8a4fd625cb
chirang56 <tamgaubijiranchi114@gmail.com>
2025-02-10 23:50:03 +0545

    feat: add navbar section to the website

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "Loading...";
+  const fullText = "Welcome...";
 
   useEffect(() => {
     let index = 0;
@@ -25,7 +25,7 @@ export const LoadingScreen = ({ onComplete }) => {
   return (
     <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col items-center justify-center">
       <div className="mb-4 text-4xl font-mono font-bold text-purple-500">{text}
-          <span className="animate-blink ml-1"> shortly </span> 
+          <span className="animate-blink ml-1"> Dear, People! </span> 
       </div>
         <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
           <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6 animate-loading-bar">
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+export const Navbar =() => {
+  return <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-white/10 shadow-lg">
+    <div className="max-w-5xl mx-auto px-4">
+      <div className="flex items-center justify-between h-16">
+        <a href="#home" className="font-mono text-xl font-bold text-white"> {""} Chee Run <span className="bg-gradient-to-br from-violet-800 to-rose-600 text-transparent bg-clip-text"> GB . Gtm </span> {""} </a>
+
+        <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden"> &#9776; 
+        </div>
+
+        <div className="hidden md:flex items-center space-x-8">
+          <a href="#home" className="text-gray-300 hover:text-white transition-colors"> {""}
+            Home{""}
+          </a>
+          <a href="#about" className="text-gray-300 hover:text-white transition-colors"> {""}
+            About{""}
+          </a>
+          <a href="#services" className="text-gray-300 hover:text-white transition-colors"> {""}
+            Services{""}
+          </a>
+          <a href="#contact" className="text-gray-300 hover:text-white transition-colors"> {""}
+            Contact{""}
+          </a>
+        </div>
+      </div>
+    </div>
+  </nav>
+}
