7c6cf209ff6e8e00bda0e140b565bcf38c205990
chirang56 <tamgaubijiranchi114@gmail.com>
2025-02-11 10:54:07 +0545

    feat: integrate mobile menu for navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,65 @@
-export const Navbar =() => {
-  return <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-white/10 shadow-lg">
-    <div className="max-w-5xl mx-auto px-4">
-      <div className="flex items-center justify-between h-16">
-        <a href="#home" className="font-mono text-xl font-bold text-white"> {""} Chee Run <span className="bg-gradient-to-br from-violet-800 to-rose-600 text-transparent bg-clip-text"> GB . Gtm </span> {""} </a>
+import { useEffect } from "react";
 
-        <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden"> &#9776; 
-        </div>
+export const Navbar = ({ menuOpen, setMenuOpen }) => {
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "";
+  }, [menuOpen]);
+  return (
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+      <div className="max-w-5xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <a href="#home" className="font-mono text-xl font-bold bg-gradient-to-br from-blue-500 to-amber-700 text-transparent bg-clip-text">
+              Chirang
+            .
+            <span className="bg-gradient-to-br from-violet-800 to-rose-600 text-transparent bg-clip-text">
+              {" "}
+              56{" "}
+            </span>
+            {" "}
+          </a>
 
-        <div className="hidden md:flex items-center space-x-8">
-          <a href="#home" className="text-gray-300 hover:text-white transition-colors"> {""}
-            Home{""}
-          </a>
-          <a href="#about" className="text-gray-300 hover:text-white transition-colors"> {""}
-            About{""}
-          </a>
-          <a href="#services" className="text-gray-300 hover:text-white transition-colors"> {""}
-            Services{""}
-          </a>
-          <a href="#contact" className="text-gray-300 hover:text-white transition-colors"> {""}
-            Contact{""}
-          </a>
+          <div
+            className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            &#9776;
+          </div>
+
+          <div className="hidden md:flex items-center space-x-8">
+            <a
+              href="#home"
+              className="text-gray-300 hover:text-fuchsia-400 transition-colors"
+            >
+              {" "}
+              Home
+            </a>
+
+            <a
+              href="#about"
+              className="text-gray-300 hover:text-fuchsia-400 transition-colors"
+            >
+              {" "}
+              About{" "}
+            </a>
+
+            <a
+              href="#projects"
+              className="text-gray-300 hover:text-fuchsia-400 transition-colors"
+            >
+              {" "}
+              Projects{" "}
+            </a>
+
+            <a
+              href="#contact"
+              className="text-gray-300 hover:text-fuchsia-400 transition-colors"
+            >
+              {" "}
+              Contact{" "}
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
-}
+    </nav>
+  );
+};
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    require("tailwindcss-scrollbar"),
-    tailwindcss(),
-    react()
-  ],
+  plugins: [react(),tailwindcss()],
 })
