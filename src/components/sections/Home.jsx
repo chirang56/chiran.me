17962b3d347c8d7d37bd764dfb1d3ce6cc73b705
chirang56 <tamgaubijiranchi114@gmail.com>
2025-02-11 12:38:28 +0545

    feat: create a hero section with some gradient text

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return (
+    <section id="home" className="flex relative items-center justify-center min-h-screen">
+      <div className="text-center z-10 px-4">
+      <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-br from-cyan-300 to-fuchsia-400 bg-clip-text text-transparent loading right">Hi, I'm Chiranjibi Gautam</h1>
+
+      <p> I'm a full-stack developer who loves crafting clean & scalable web applications and mobile apps with a delightful user experience. I'm a part-time media translator and subtitler too. </p>
+      </div>
+    </section>
+  );
+};
