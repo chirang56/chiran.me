698a4464966961ba5047c50dad7f0000b1acc283
chirang56 <tamgaubijiranchi114@gmail.com>
2025-02-12 00:01:20 +0545

    fix: add backend skills to the skill section in the about section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -26,8 +26,20 @@ export const About = () => {
                   ))}
                 </div>
               </div>
+
+              <div className="rounded-xl p-6 hover:-translate-y-1 transiton-all">
+                <h3 className="text-xl font-bold mb-4"> Backend </h3>
+                <div className="flex flex-wrap gap-2">
+                  {backendSkills.map((tech, key) => (
+                    <span key={key} className="bg-blue-500/10 text-blue-500 px-3 py-1 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
+          
       </div>
     </section>
   );     
