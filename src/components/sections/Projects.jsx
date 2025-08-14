3c9bac982785d2c726289b68cc0f40df8c765c79
chirang56 <tamgaubijiranchi114@gmail.com>
2025-08-14 10:06:47 +0545

    content: revise project descriptions for clarity

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -34,7 +34,7 @@ export const Projects = () => {
   const projects = [
     {
       title: "Personal Portfolio",
-      description: "Personal Portfolio Website showcasing all the needed information for any company or person in order to consider hiring me.",
+      description: "Personal Portfolio Website showcasing all the needed information for any company or person in order to consider hiring me. Developed as the first project to showcase my skills.",
       techStack: ["HTML", "CSS", "JavaScript"],
       link: "https://chirang56me.netlify.app/"
     },
@@ -45,8 +45,8 @@ export const Projects = () => {
       link: "https://matdash-frontend.vercel.app/"
     },
     {
-      title: "Gautam Steelworks",
-      description: "A responsive website showcasing Gautam Steelworks' products and services.",
+      title: "Butwal Fiber & Metal Industry",
+      description: "A responsive website showcasing Butwal Fiber & Metal Industry's products and services.",
       techStack: ["Next.js", "Tailwind CSS", "Node.js", "Express.js", "MySQL"],
       link: "https://gautam-steelworks.vercel.app/en"
     },
